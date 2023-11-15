6609c6f3e3659882b07155f800fafd4129d9944a
Profiremu23 <profirdani23@gmail.com>
2023-11-15 19:42:40 +0100

    Added a WIP working definition method

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,2 +1,9 @@
-def substrings(word, substring)
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+def substrings(string, dictionary)
+  dictionary.each do |word|
+    p string.downcase.scan(/#{word}/)
+  end
 end
+
+substrings("Howdy partner, how are you doing?", dictionary)
