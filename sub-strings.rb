d851f1a8e1bcb5d7550095c236e499d3b0fbfba1
Profiremu23 <profirdani23@gmail.com>
2023-11-20 19:54:42 +0100

    Fixed the substring result returning method

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,9 +1,11 @@
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
 def substrings(string, dictionary)
+  result = Array.new
   dictionary.each do |word|
-    p string.downcase.scan(/#{word}/)
+    result.push(string.downcase.scan(/#{word}/))
   end
+  p result.flatten.tally
 end
 
 substrings("Howdy partner, how are you doing?", dictionary)
